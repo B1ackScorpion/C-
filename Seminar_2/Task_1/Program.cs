75ae8738fa40df6206121c4de5cfdfadd0b0dbdf
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-05 18:25:14 +0200

    add task_1

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+
+Console.Clear();
+Console.Write("Введите 3-х значное число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n); // Модуль числа
+while (n < 100 || n > 999){
+    Console.Write("Вы ошиблись!\nВведите 3-х значное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
+Console.WriteLine($"{n1}{n3}");
