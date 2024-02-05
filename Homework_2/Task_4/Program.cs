3751b03c1f149315e59f82c25b778c05f5b81b27
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-05 20:05:29 +0200

    add task_4

---
+++ Homework_2/Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4.
+// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1){
+    Console.Write("Число не является натуральным\nВведите натуральное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int m = n;
+int i = 1;
+while (n > 10){
+    while (m > 10){
+        m /= 10;
+        i *= 10;
+    }
+    Console.Write($"{m}, ");
+    n -= m * i;
+    m = n;
+    i = 1;
+}
+Console.WriteLine(n);
