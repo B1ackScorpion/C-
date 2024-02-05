1c0e984711df5354a0aa4fbcb5a461445b31d0e8
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-05 18:53:30 +0200

    add task_4

---
+++ Seminar_2/Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задание 4
+// Напишите программу, которая выводит третью с конца
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n);
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет");
+else{
+    int x = n % 1000;
+    Console.WriteLine(x / 100);
+}
