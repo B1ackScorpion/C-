87c7d77a7d8177faeff8de1c3cd07380c754aeb4
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-05 18:45:39 +0200

    add task_2

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 2
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+
+Console.Clear();
+Console.Write("Введите 3-х значное число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n); // Модуль числа
+while (n < 100 || n > 999){
+    Console.Write("Вы ошиблись!\nВведите 3-х значное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int n3 = n % 10;
+int n2 = (n % 100) / 10;
+// Console.WriteLine(Math.Pow(n2, n3));
+int result = 1;
+for (int i = 0; i < n3; i++)
+    result *= n2;
+
+Console.WriteLine($"{n2}^{n3} = {result}");
