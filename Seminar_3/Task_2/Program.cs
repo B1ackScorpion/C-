92e260c313c5929222d76f6c495a1288691190ee
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-06 19:34:31 +0200

    add task_2

--- Seminar_3/Task_2/Program.cs
+++ Seminar_3/Task_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 2
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+
+Console.Clear();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11); // [-10; 10]
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+for (int i = 0; i < array.Length; i++)
+    array[i] *= -1;
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
