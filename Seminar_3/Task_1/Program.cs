0bd080d9b488ccd319c04ad16132c6206c0b1ed7
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-06 19:30:53 +0200

    add task_1

--- Seminar_3/Task_1/Program.cs
+++ Seminar_3/Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+
+ Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];   // [0, 0, ..., 0]
+// double[] array = new double[n]; // [0.0, 0.0, ..., 0.0]
+// string[] array = new string[n]; // ["", "", ..., ""]
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11); // [-10; 10]
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+Console.Write("Введите искомый элемент: ");
+int element = int.Parse(Console.ReadLine()!);
+string result = "Нет";
+for (int i = 0; i < array.Length; i++){
+    if (array[i] == element)
+        result = "Да";
+}
+Console.WriteLine(result);
