155f6e24386a7fa8a7084690be0eb5e5f2381633
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-09 20:24:07 +0200

    add task_2

--- Seminar_4/Task_2/Program.cs
+++ Seminar_4/Task_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1001);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int count = 0;
+foreach (int element in array)
+{
+    if (element % 10 == 1 && element % 7 == 0)
+        count++;
+}
+Console.Write(count);
