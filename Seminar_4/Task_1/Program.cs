f7f6947e1479063fdcaa301b04c8633dd94caefb
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-09 20:15:52 +0200

    add task_1

--- Seminar_4/Task_1/Program.cs
+++ Seminar_4/Task_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1.
+// Задайте одномерный массив, заполненный случайными
+// числами. Определите количество простых чисел в этом
+// массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1001);
+    }
+}
+
+bool IsPrime(int x)
+{
+    for (int i = 2; i <= x / 2; i++)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+foreach (int element in array)
+{
+    if (IsPrime(element))
+        Console.Write($"{element} ");
+}
