54ad5491417db5b8934a33ff56696a81f4227389
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-10 15:41:15 +0200

    add task_3

--- Homework_4/Task_3/Program.cs
+++ Homework_4/Task_3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3. 
+// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+void InputArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void NewArray (int[] array)
+{
+    int m = array.Length - 1;
+    int k = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        k = array[i];
+        array[i] = array[i+m];
+        array[i+m] = k;
+        m -= 2;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Ссозданный массив: [{string.Join(", ", array)}]");
+NewArray(array);
+Console.WriteLine($"Перевернутый массив: [{string.Join(", ", array)}]");
