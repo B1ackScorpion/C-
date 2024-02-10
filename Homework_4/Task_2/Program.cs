458df4a6ce0fdb9c65eef85fb86efaa9d284af4b
Daniil Aznabaev <dan.aznob@gmail.com>
2024-02-10 15:09:43 +0200

    add task_2

--- Homework_4/Task_2/Program.cs
+++ Homework_4/Task_2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2.
+// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во чисел");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+int count = EvenNumbers(array);
+Console.WriteLine($"Ссозданный массив: [{string.Join(", ",array)}]");
+Console.WriteLine($"Кол-во четных чисел в массиве: {count}");
